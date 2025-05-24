f2dcca9833bed645dee855e9ddbdf3d4c01874f9
mikabrytu <mikabrytu@gmail.com>
2025-05-24 20:53:35 -0300

    Setup game screen

---
+++ values/number.go
@@ -0,0 +1,10 @@
+package values
+
+import "github.com/mikabrytu/gomes-engine/math"
+
+var (
+	SCREEN_SIZE = math.Vector2{
+		X: 800,
+		Y: 600,
+	}
+)
---
+++ values/string.go
@@ -0,0 +1,5 @@
+package values
+
+const (
+	GAME_TITLE string = "Gong"
+)
